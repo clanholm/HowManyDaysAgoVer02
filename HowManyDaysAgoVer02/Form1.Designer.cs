ee598f07f63caaf3886becd0c782344ead940898
Chuck <Chuck@CHUCK-7550>
2021-10-11 16:16:42 -0400

    Add project files.

---
+++ HowManyDaysAgoVer02/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace HowManyDaysAgoVer02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.lblDisplayDays = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(46, 12);
+            this.dateTimePicker1.MaxDate = new System.DateTime(2199, 12, 31, 0, 0, 0, 0);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 26);
+            this.dateTimePicker1.TabIndex = 0;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // lblDisplayDays
+            // 
+            this.lblDisplayDays.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDisplayDays.Location = new System.Drawing.Point(12, 50);
+            this.lblDisplayDays.Name = "lblDisplayDays";
+            this.lblDisplayDays.Size = new System.Drawing.Size(272, 206);
+            this.lblDisplayDays.TabIndex = 1;
+            this.lblDisplayDays.Text = "Choose a date to calculate how many days between that date and today.";
+            this.lblDisplayDays.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(296, 265);
+            this.Controls.Add(this.lblDisplayDays);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(2);
+            this.Text = "How Many Days";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label lblDisplayDays;
+    }
+}
+
---
+++ HowManyDaysAgoVer02/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HowManyDaysAgoVer02
+{
+    public partial class Form1 : Form
+    {
+        DateTime myDate = new DateTime();
+        
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            double nDaysAgo;
+            string strDaysAgo;
+            string strMessageHeader;
+            string strMessageTail;
+            string strDayOfWeek;
+
+            myDate = dateTimePicker1.Value;
+            strDayOfWeek = myDate.ToString("D");
+            TimeSpan myAge = DateTime.Now.Subtract(myDate);
+
+            strDaysAgo = string.Format("{0:N0}", myAge.TotalDays);
+            nDaysAgo = Convert.ToDouble(strDaysAgo);
+
+            if(nDaysAgo == 0)
+            {
+                strMessageHeader = " Is Today!";
+                strDaysAgo = "";
+                strMessageTail = "";
+                lblDisplayDays.ForeColor = Color.Blue;
+            }
+
+            else if(nDaysAgo == 1)
+            {
+                strMessageHeader = " Was Yesterday!";
+                strDaysAgo = "";
+                strMessageTail = "";
+                lblDisplayDays.ForeColor = Color.DarkGreen;
+            }
+
+            else if(nDaysAgo == -1)
+            {
+                strMessageHeader = " is Tomorrow!";
+                strDaysAgo = "";
+                strMessageTail = "";
+                lblDisplayDays.ForeColor = Color.DarkRed;
+            }
+
+            else if(nDaysAgo < -1)
+            {
+                strMessageHeader = " Occurs ";
+                strDaysAgo = Math.Abs(nDaysAgo).ToString();
+                strMessageTail = " Days from Today!";
+                lblDisplayDays.ForeColor = Color.DarkRed;
+            }
+
+            else
+            {
+                strMessageHeader = " Occured ";
+                strMessageTail = " Days Ago!";
+                lblDisplayDays.ForeColor = Color.DarkGreen;
+            }
+
+            lblDisplayDays.Text = strDayOfWeek + strMessageHeader + strDaysAgo + strMessageTail;
+        }
+    }
+}
